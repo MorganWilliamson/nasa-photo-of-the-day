29d2d15b0be3240a9be209208c8dd73a19c1217c
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-07 15:38:41 -0500

    Added import statements and slices of state in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import Header from "./components/header";
+import Date from "./components/date";
+import Copyright from "./components/copyright";
+import Photo from "./components/photo";
+import Info from "./components/info";
 
 function App() {
+const [title, setTitle] = useState("");
+const [date, setDate] = useState("");
+const [copyright, setCopyright] = useState("");
+const [imageURL, setImageURL] = useState("");
+const [info, setInfo] = useState("");
+
   return (
     <div className="App">
       <p>
