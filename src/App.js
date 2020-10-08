d6e5908b18ee2a0e101372bbce77e567a63e5092
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-07 20:42:29 -0500

    Adding placeholder text for page.

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ function App() {
 const [nasaData, setNasaData] = useState([]); 
 
 //Create a function (axios) that fetches the data from the NASA API.
-useEffect(() => {
-  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14')
-  .then(res => {
-    setNasaData(res.data)
-  })
-  .catch(err => {
-    console.log(err);
-  })
 
-}, [])
+//Remember to un-comment this or the page won't work. 
+// useEffect(() => {
+//   axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14')
+//   .then(res => {
+//     setNasaData(res.data)
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   })
+
+// }, [])
 
 //This section should be everything returning/appending to the DOM? 
   return (
@@ -28,3 +30,9 @@ useEffect(() => {
 }
 
 export default App;
+
+
+/* Ignore this. 
+Base image URL: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+Image URL with 2012 date: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14'
+*/
--- src/components/nasaImage.js
+++ src/components/nasaImage.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 function NasaImage(props){
+    //display array so we know what data we're working with
     console.log(props);
 //build out the proper JSX for the page here
-return <div> 
-    <p>{props.nasaData.date}</p>
-    <h2>{props.nasaData.title}</h2>
-    <h2>{props.nasaData.copyright}</h2>
-    <img src={props.nasaData.url} alt="Current APOD"/> 
-    <p>{props.nasaData.explanation}</p>
+return <div className="NIContainer"> 
+        <p className="NIDate">Date: {props.nasaData.date}</p>
+            <h2 className="NITitle">Tite: {props.nasaData.title}</h2>
+                <h2 className="NICopyright">Taken by: {props.nasaData.copyright}</h2>
+            <img className="NIImage" src={props.nasaData.url} alt="Current APOD"/> 
+        <p className="NIDescrip">Information: {props.nasaData.explanation}</p>
     </div>;
 };
 
-
+//maybe add a button so the information is hidden on load, but shows if they click a button
 
 export default NasaImage;
